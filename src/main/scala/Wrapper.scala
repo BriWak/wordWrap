f3b25e4c6401024faad7f59f843da72ea52c79a1
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-08-20 14:07:22 +0100

    wordWrap: Refactor wrap method to be tail recursive

--- src/main/scala/Wrapper.scala
+++ src/main/scala/Wrapper.scala
@@ -1,15 +1,21 @@
+import scala.annotation.tailrec
 
 object Wrapper {
 
   def wrap(textToWrap: String, col: Int): String = {
-    val (firstPart, secondPart) = textToWrap.splitAt(col)
-    val spaceLocation: Int = firstPart.lastIndexOf(' ')
-    val (colOne, colTwo) = if (firstPart.contains(' ')) textToWrap.splitAt(spaceLocation) else (firstPart, secondPart)
+    @tailrec
+    def wrapHelper(textToWrap: String, col: Int, acc: List[String]): List[String] = {
+      val (firstPart, secondPart) = textToWrap.splitAt(col)
+      val spaceLocation: Int = firstPart.lastIndexOf(' ')
+      val (colOne, colTwo) = if (firstPart.contains(' ')) textToWrap.splitAt(spaceLocation) else (firstPart, secondPart)
 
-    if (textToWrap.length <= col)
-      textToWrap
-    else
-      s"${colOne.trim}\n${wrap(colTwo.trim, col)}"
+      if (textToWrap.length <= col)
+        acc :+ textToWrap
+      else
+        wrapHelper(colTwo.trim, col, acc :+ colOne.trim)
+    }
+
+    wrapHelper(textToWrap, col, Nil).mkString("\n")
   }
 
 }
